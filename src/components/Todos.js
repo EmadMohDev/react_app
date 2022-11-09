fb5270735c3ded72cf3d631503474fe15707ab9b
emad.mohamed.ivas <emad@ivas.com.eg>
2022-11-09 16:07:38 +0200

    react-37:46

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,12 +3,14 @@ import { Component } from 'react';
 class Todos extends Component {
     render() {
         return (
-            <h1> Todos component </h1>
+            <h1> Todos component update</h1>
         );
 
     };
 
 }
+
+
 
 export default Todos;
 
