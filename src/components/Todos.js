3ca8ca75eef1aed14ad0db6ff2192915d5b63719
emad.mohamed.ivas <emad@ivas.com.eg>
2022-11-09 16:05:44 +0200

    Initial commit

---
+++ src/components/Todos.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class Todos extends Component {
+    render() {
+        return (
+            <h1> Todos component </h1>
+        );
+
+    };
+
+}
+
+export default Todos;
+
+
